4a904e5e6015799e2b3219fa1a63f7a9fa40e927
eric white <ericwhite@ericdeMacBook-Pro.local>
2022-11-26 09:57:55 +0800

    updates

--- proxy.c
+++ proxy.c
@@ -7,7 +7,7 @@
 #define MAX_OBJECT_SIZE 102400
 #define PORT "38888"
 /* You won't lose style points for including this long line in your code */
-static const char *user_agent_hdr = "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 Firefox/10.0.3";
+static const char *user_agent_hdr = "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 Firefox/10.0.3\r\n";
 static const char *conn_hdr = "Connection: close\r\n";
 static const char *proxy_conn_hdr = "Proxy-Connection: close\r\n";
 int transmit(int connfd);
